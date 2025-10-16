0218bd98a1dbe12ec9d15b8a8f87fb5348a0b97d
Vito <hongcuongcl98@gmail.com>
2025-10-16 17:41:53 +0700

    Initial commit: Spring Microservices Blueprint

---
+++ account-service/src/main/java/com/demo/payload/request/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.demo.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/**
+ * @author Vito Nguyen (<a href="https://github.com/cuongnh28">...</a>)
+ */
+
+@Data
+public class LoginRequest {
+
+	@NotBlank(message = "Username is required")
+  	private String username;
+
+	@NotBlank(message = "Password is required")
+	private String password;
+
+}
